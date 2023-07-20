53c3cc4e50b1b1ca70f6c4b887849be2cdfdd349
Pedro Resende <pedroluizrnunez@gmail.com>
2023-07-20 12:11:32 -0300

    requisito 8 completo

---
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionControllerTest.java
@@ -0,0 +1,46 @@
+package com.betrybe.museumfinder.solution;
+
+import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.service.CollectionTypeService;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Testes da CollectionController")
+public class CollectionControllerTest {
+
+  @MockBean
+  CollectionTypeService collectionTypeService;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  @DisplayName("Rota GET /collection/count/{typeList} implementada")
+  void testGetCollectionTypesCount() throws Exception {
+    String[] collectionTypes = new String[]{"história"};
+    CollectionTypeCount collectionTypeCount = new CollectionTypeCount(collectionTypes, 387);
+    Mockito.when(collectionTypeService.countByCollectionTypes("história"))
+        .thenReturn(collectionTypeCount);
+
+    mockMvc.perform(get("/collections/count/história"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.count").value(387))
+        .andExpect(jsonPath("$.collectionTypes").value(collectionTypes[0]));
+
+  }
+
+}
---
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -0,0 +1,39 @@
+package com.betrybe.museumfinder.solution;
+
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.service.CollectionTypeService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+@DisplayName("Testes da CollectionTypeService")
+public class CollectionTypeServiceTest {
+
+  @MockBean
+  MuseumFakeDatabase museumFakeDatabase;
+
+  @Autowired
+  CollectionTypeService service;
+
+  @Test
+  @DisplayName("testa countByCollectionTypes")
+  void testCountByCollectionTypes() {
+    Mockito.when(museumFakeDatabase.countByCollectionType("história"))
+        .thenReturn(387L);
+    String[] type = new String[]{"história"};
+    CollectionTypeCount collectionTypeCount = new CollectionTypeCount(type, 387l);
+
+    Assertions.assertEquals(collectionTypeCount.count(),
+        service.countByCollectionTypes("história").count());
+    Assertions.assertEquals(collectionTypeCount.collectionTypes()[0],
+        service.countByCollectionTypes("história").collectionTypes()[0]);
+  }
+
+}
