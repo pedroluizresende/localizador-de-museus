052586c4e600e0c0666e9e0694c30ec6d38e9a37
Pedro Resende <pedroluizrnunez@gmail.com>
2023-07-19 22:07:14 -0300

    initial commit

--- src/main/java/com/betrybe/museumfinder/MuseumFinderApplication.java
+++ src/main/java/com/betrybe/museumfinder/MuseumFinderApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MuseumFinderApplication {
 
   public static void main(String[] args) {
-//
     SpringApplication.run(MuseumFinderApplication.class, args);
   }
 }
